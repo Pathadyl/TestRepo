abff7581848bbee3510b1565a461386c0d7eff16
Dylan <122667165+Pathadyl@users.noreply.github.com>
2025-02-09 17:19:19 +0700

    chore(gitignore): ignore unnecessary files and folders v4

--- HelloWorld/src/Main.java
+++
@@ -1,8 +0,0 @@
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Quãng đường = " + (float) 14 / 1.6 + " dam");
-        System.out.println("Thời gian = " + (float) (45 * 60 + 30) / 3600 + " h");
-        System.out.println("Tốc độ trung bình = " + ((float) 14 / 1.6) / ((float) (45 * 60 + 30) / 3600) + " dam/h");
-    }
-}
